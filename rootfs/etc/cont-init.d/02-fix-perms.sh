963dfaa316b8ed5fabacdce966b1dfee73821bae
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-08-27 10:18:48 +0200

    only chown subdirs of vendor directory

--- rootfs/etc/cont-init.d/02-fix-perms.sh
+++ rootfs/etc/cont-init.d/02-fix-perms.sh
@@ -6,14 +6,20 @@ echo "Fixing perms..."
 mkdir -p /data \
   /var/run/nginx \
   /var/run/php-fpm
+
 chown librenms:librenms \
   /data \
   "${LIBRENMS_PATH}" \
-  "${LIBRENMS_PATH}/.env" \
-  "${LIBRENMS_PATH}/cache" \
-  "${LIBRENMS_PATH}/rrd"
+  "${LIBRENMS_PATH}"/.env \
+  "${LIBRENMS_PATH}"/cache \
+  "${LIBRENMS_PATH}"/composer* \
+  "${LIBRENMS_PATH}"/rrd \
+  "${LIBRENMS_PATH}"/vendor \
+  "${LIBRENMS_PATH}"/vendor/* \
+
 chown -R librenms:librenms \
   /home/librenms \
+  "${LIBRENMS_PATH}"/vendor/composer \
   /tpls \
   /var/lib/nginx \
   /var/log/nginx \
